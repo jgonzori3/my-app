28e088195262bf9793bae08999563630955d4f94
jgonzori3 <47305742+jgonzori3@users.noreply.github.com>
2020-12-05 11:38:13 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. hello world
         </p>
         <a
           className="App-link"
@@ -19,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <h1>hola hola caracola</h1>
     </div>
   );
 }
